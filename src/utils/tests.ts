c53db88e12a8f634fda9218129a725ca10943de9
Roko Čupić <rcupic@profico.hr>
2024-04-09 15:15:44 +0200

    Rename function name to reflect that only txt files are read

--- src/utils/tests.ts
+++ src/utils/tests.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-export function readAllFilesInDir(
+export function readAllTxtFilesInDir(
   dir: string,
   filenamesToIgnore: string[] = [],
 ): string[] {
